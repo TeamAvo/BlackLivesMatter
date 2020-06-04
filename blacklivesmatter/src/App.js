9ad8cd9435068bfd7223a10f714b22d7ff49b8fe
Spencilian <spencerli253@gmail.com>
2020-06-04 16:59:39 -0400

    Added icons and images

--- blacklivesmatter/src/App.js
+++ blacklivesmatter/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
     </div>
   );
 }
 
-export default App;
+export default App
