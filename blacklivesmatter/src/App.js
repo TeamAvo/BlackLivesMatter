4b32139d5196bc542041cb58bd6864e08d88f0db
Nitro <nitro0@naver.com>
2020-06-06 01:38:23 +0900

    Dev environment test.

--- blacklivesmatter/src/App.js
+++ blacklivesmatter/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Test from './components/Test'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
       <Header />
+      <Test />
     </div>
   );
 }
---
+++ blacklivesmatter/src/components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test(){
+    return(
+        <h1>Protest should be powerful but peaceful.</h1>
+    )
+}
+
+export default Test
