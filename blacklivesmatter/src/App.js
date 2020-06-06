dcb26a49ccd851140c706122955dbb8ae9e836a4
Spencilian <spencerli253@gmail.com>
2020-06-06 03:41:30 -0400

    Adding Header

--- blacklivesmatter/src/App.js
+++ blacklivesmatter/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
