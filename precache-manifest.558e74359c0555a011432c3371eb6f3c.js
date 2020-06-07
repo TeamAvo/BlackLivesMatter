0cac7e43fdeb041db81960cd2b7f5f46fa9969da
Nitro <nitro0@naver.com>
2020-06-07 19:16:43 +0900

    Updates

---
+++ precache-manifest.558e74359c0555a011432c3371eb6f3c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533613755e59a7cb922fc027f11bae76",
+    "url": "/BlackLivesMatter/index.html"
+  },
+  {
+    "revision": "ccd366c6dd55217e590c",
+    "url": "/BlackLivesMatter/static/css/main.f7d4ba38.chunk.css"
+  },
+  {
+    "revision": "8f0a8eafb4062f9d1d9d",
+    "url": "/BlackLivesMatter/static/js/2.f20cab22.chunk.js"
+  },
+  {
+    "revision": "ccd366c6dd55217e590c",
+    "url": "/BlackLivesMatter/static/js/main.34a24070.chunk.js"
+  },
+  {
+    "revision": "e43bdb5d970dc1834087",
+    "url": "/BlackLivesMatter/static/js/runtime~main.f20eb454.js"
+  },
+  {
+    "revision": "2323a1567286eb2246cdffa82e6c0b6c",
+    "url": "/BlackLivesMatter/static/media/Cornerstone.2323a156.woff2"
+  },
+  {
+    "revision": "2c7c14a6157c7e7e7b8512661dcad6a7",
+    "url": "/BlackLivesMatter/static/media/Cornerstone.2c7c14a6.woff"
+  },
+  {
+    "revision": "ac83ef5aaa1bab77cde1c44ad906f4b8",
+    "url": "/BlackLivesMatter/static/media/Cornerstone.ac83ef5a.ttf"
+  },
+  {
+    "revision": "47dee870a4072c4d0523aa0c342932af",
+    "url": "/BlackLivesMatter/static/media/blm_icon.47dee870.png"
+  },
+  {
+    "revision": "96d3b45d50a8b2e5e90baad44b3fbbbc",
+    "url": "/BlackLivesMatter/static/media/blm_v1.96d3b45d.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/BlackLivesMatter/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/BlackLivesMatter/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/BlackLivesMatter/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/BlackLivesMatter/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
