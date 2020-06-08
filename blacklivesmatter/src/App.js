fd618527d37aad7ee8e68d00598dd52b04f6c76a
Spencilian <spencerli253@gmail.com>
2020-06-08 09:14:21 -0400

    Created Starter project and added depencies

--- blacklivesmatter/src/App.js
+++ blacklivesmatter/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Map from "./Components/Map";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <Map/>
       </div>
     );
   }
---
+++ blacklivesmatter/src/Components/Map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    render(){
+        return (
+            <h1>Map</h1>
+        );
+    }
+}
+
+export default Map
