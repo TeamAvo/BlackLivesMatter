3e5569f04b92fd5be628acf346e8c96a0d5da16a
Spencilian <spencerli253@gmail.com>
2020-06-08 09:43:25 -0400

    Add Test Map

--- blacklivesmatter/src/App.js
+++ blacklivesmatter/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-import Map from "./Components/Map";
+import MapChart from "./Components/Map";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
-        <Map/>
+        <MapChart/>
       </div>
     );
   }
